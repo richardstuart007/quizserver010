a0ff9cad18906cfdcf43f3833598c2da93861b46
Richard <richardstuart007@hotmail.com>
2022-10-31 20:42:52 +1300

    users update

--- controllers/serverRegister.js
+++ controllers/serverRegister.js
@@ -3,7 +3,8 @@ const serverRegister = (req, res, db) => {
   //..............................................................................
   //.  Check the data sent
   //.............................................................................
-  const { email, name, password, fedid } = req.body
+  const { email, name, password, fedid, fedcountry } = req.body
+  console.log(email, name, password, fedid, fedcountry)
 
   if (!email || !name || !password) {
     console.log('Email or Name or Password empty')
@@ -33,6 +34,13 @@ const serverRegister = (req, res, db) => {
             u_email: email,
             u_admin: false,
             u_fedid: fedid,
+            u_fedcountry: fedcountry,
+            u_showprogress: true,
+            u_showscore: true,
+            u_sortquestions: true,
+            u_skipcorrect: true,
+            u_dftmaxquestions: 5,
+            u_dftowner: 'NZBridge',
             u_joined: new Date()
           })
           .then(user => {
